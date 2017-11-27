9b9b2a5b7de313d8414d1a0a44cb3c00f23f0749
Tam Le <hereistam@gmail.com>
2017-11-27 12:55:47 -0800

    Added signalr

---
+++ Automobility.Web/Scripts/Controllers/SignalRController.js
@@ -0,0 +1,34 @@
+﻿// SignalR CONTROLLER
+
+(function () {
+    'use strict';
+
+    angular
+        .module(AppName)
+        .controller("signalRContoller", SignalRController);
+
+    SignalRController
+        .$inject = ["$scope"];
+
+    function SignalRController($scope) {
+        var vm = this;
+        vm.$scope = $scope;
+
+        vm.hackHub = null;
+
+        vm.$scope.hackHub = $.connection.hack;
+
+        console.log('initializing signalr');
+
+        $.connection.hub.logging = true;
+
+        $.connection.hub
+            .start()
+            .done(connectedToSignalR)
+            .fail(function () { console.error('Error connecting to SignalR'); }); 
+
+        function connectedToSignalR() {
+            console.log('Connected to SignalR, connection ID: ' + $.connection.hub.id);
+        };
+    };
+})();
