af48dbe504f67fcce7ef7d95d8c108d835c4c2aa
James Nguyen <jaywin89@gmail.com>
2017-11-27 22:17:29 -0800

    updating with controller

---
+++ Automobility.Web/Templates/LocationSchedule.js
@@ -0,0 +1,20 @@
+﻿<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8" />
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+                <title></title>
+</head>
+            <body>
+                <div class="widget-body" ng-app="HondaHack" ng-controller="itinController as ic">
+                </div>
+
+                <script src="../Scripts/jquery-3.1.1.min.js"></script>
+                <script src="../Scripts/bootstrap.min.js"></script>
+                <script src="../Scripts/angular.min.js"></script>
+                <script src="../Scripts/Site/app.js"></script>
+                <script src="../Scripts/Controller/ItineraryController.js"></script>
+
+
+            </body>
+</html>
